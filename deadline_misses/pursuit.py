25bad41ed857d51da3fd1e27b0f1055c79a83c18
shengjiex98 <shengjiex98@gmail.com>
2022-03-24 14:08:53 -0400

    Implement path rendering and deadline miss handling

--- deadline_misses/pursuit.py
+++ deadline_misses/pursuit.py
@@ -1,3 +1,4 @@
+import collections
 import time
 import yaml
 import gym
@@ -6,7 +7,7 @@ from argparse import Namespace
 
 from numba import njit
 
-from pyglet.gl import GL_POINTS, GL_LINES
+from pyglet.gl import GL_POINTS, GL_LINES, GL_LINE_STRIP
 
 class PurePursuitPlanner:
     """
@@ -15,64 +16,73 @@ class PurePursuitPlanner:
     def __init__(self, conf, wb):
         self.wheelbase = wb
         self.conf = conf
-        self.max_reacquire = 20.
+        self.max_reacquire = 20
 
-        self.path = None
+        # Used for deadline misses handling
+        self.last = 0
+        self.miss_last = False
+        self.saved_state = None
+
+        self.path = collections.deque(maxlen=500)
         self.drawn_path = []
-
-    def render_waypoints(self, e):
-        """
-        update waypoints being drawn by EnvRenderer
-        """
-        return
-        #points = self.waypoints
-
-        points = np.vstack((self.waypoints[:, self.conf.wpt_xind], self.waypoints[:, self.conf.wpt_yind])).T
-        
-        scaled_points = 50.*points
-
-        for i in range(points.shape[0]):
-            if len(self.drawn_waypoints) < points.shape[0]:
-                b = e.batch.add(1, GL_POINTS, None, ('v3f/stream', [scaled_points[i, 0], scaled_points[i, 1], 0.]),
-                                ('c3B/stream', [183, 193, 222]))
-                self.drawn_waypoints.append(b)
-            else:
-                self.drawn_waypoints[i].vertices = [scaled_points[i, 0], scaled_points[i, 1], 0.]
 
     def render_path(self, e):
         """
         Renders the path that the vehicle has followed
         """
-        if self.path is None:
+        if len(self.path) == 0:
             return
 
-        points = np.vstack((self.path[:, 0], self.path[:, 1])).T
+        # points = [self.path[x] for x in range(0, len(self.path), 5)]
+        points = self.path
+        points = np.array(points)
         
         scaled_points = 50.*points
 
         for i in range(points.shape[0]):
             if len(self.drawn_path) < points.shape[0]:
-                b = e.batch.add(1, GL_LINES, None, ('v3f/stream', [scaled_points[i, 0], scaled_points[i, 1], 0.]),
+                b = e.batch.add(1, GL_LINE_STRIP, None, ('v3f/stream', [scaled_points[i, 0], scaled_points[i, 1], 0.]),
                                 ('c3B/stream', [183, 193, 222]))
                 self.drawn_path.append(b)
             else:
                 self.drawn_path[i].vertices = [scaled_points[i, 0], scaled_points[i, 1], 0.]
 
-    def plan(self, pose_x, pose_y, pose_theta, lookahead_distance, vgain):
+    def plan(self, pose_x, pose_y, pose_theta, lookahead_distance, miss=False, hold=True, kill=True):
         """
         gives actuation given observation
         """
-        if self.path is None:
-            self.path = np.array([[pose_x, pose_y]])
-        else:
-            self.path = np.vstack((self.path, [pose_x, pose_y]))
+        # Save path for rendering
+        self.path.append([pose_x, pose_y])
 
-        speed = 6.5
-        tan_delta = ((2 * self.wheelbase / lookahead_distance ** 2) * 
-                     (-np.sqrt(lookahead_distance ** 2 - pose_y ** 2) * np.sin(pose_theta) - pose_y * np.cos(pose_theta)))
+        # Calculates the steering angle
+        tan_delta = ((2 * self.wheelbase / lookahead_distance**2) * 
+                     (-np.sqrt(lookahead_distance**2 - pose_y**2) * np.sin(pose_theta) - pose_y * np.cos(pose_theta)))
         steering_angle = np.arctan(tan_delta)
 
-        return speed, steering_angle
+        # Handle misses
+        if kill:
+            if miss:
+                result = self.last if hold else 0
+            else:
+                result = steering_angle
+        else:
+            # Skip-next
+            if miss:
+                result = self.last if hold else 0
+                if not self.miss_last:
+                    # HM
+                    self.saved_state = steering_angle
+            else:
+                if not self.miss_last:
+                    # HH
+                    result = steering_angle
+                else:
+                    # MH
+                    result = self.saved_state
+        self.last = result
+        self.miss_last = miss
+
+        return 6.5, result
 
 def main():
     work = {
@@ -119,8 +129,9 @@ def main():
 
     while not done:
         if not count:
-            speed, steer = planner.plan(obs['poses_x'][0], obs['poses_y'][0], obs['poses_theta'][0], work['tlad'], work['vgain'])
-        # count = (count + 1) % 6
+            speed, steer = planner.plan(obs['poses_x'][0], obs['poses_y'][0], obs['poses_theta'][0], 
+                                        work['tlad'], miss=False, hold=True, kill=True)
+        count = (count + 1) % 17
         obs, step_reward, done, info = env.step(np.array([[steer, speed]]))
         laptime += step_reward
         env.render(mode='human')
